bcc5e272ccb00f654e8bebecccb8aaa7ed4707d3
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-04-15 12:18:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,7 @@
+variable "project-id" {}
+
 variable "google_creds" {
   description = "GCP Service Account JSON credentials"
   type        = string
   sensitive   = true
-}
-
-provider "google" {
-    project = var.projectid
-    region = "us-central1"
-credentials = var.google_creds
 }
