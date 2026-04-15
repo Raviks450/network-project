7e1e876fb3e75069a899e4618510357802a72905
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-04-15 11:51:52 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,13 @@
+resource "google_compute_network" "vpc" {
+  project                 = var.project-id
+  name                    = "my-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  project       = var.project-id
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc.id
+}
