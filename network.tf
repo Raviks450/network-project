e2e0775babdaf962ed8be765e1541fe650bf255e
Raviks450 <127187295+Raviks450@users.noreply.github.com>
2026-04-15 12:20:25 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,3 +1,11 @@
+variable "project-id" {}
+
+variable "google_creds" {
+  description = "GCP Service Account JSON credentials"
+  type        = string
+  sensitive   = true
+}
+
 provider "google" {
     project = var.projectid
     region = "us-central1"
